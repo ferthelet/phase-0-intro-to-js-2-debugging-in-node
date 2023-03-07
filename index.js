45dd57fa33ace4a16e703d7fbac4e9c077f68ced
Juan Salazar <ferthelet@gmail.com>
2023-03-07 13:47:21 -0600

    adds one breakpoint

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+// Node.js (which is the runtime environment that enables us
+// to run our JavaScript outside the browser) 
+// js
+
 let count = 1;
 while (count < 5) {
+  debugger;
   if (count === 1) {
     console.log(`The loop has executed ${count} time.`);
   } else {
